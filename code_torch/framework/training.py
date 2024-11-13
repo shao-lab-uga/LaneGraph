a5710236bdc940f9d6c9e4dc4fbdab5f51664243
Yunli Shao <yunli.shao@uga.edu>
2024-11-13 01:42:06 -0500

    have a flag to not import tensorflow at all and only use torch

--- code_torch/framework/training.py
+++ code_torch/framework/training.py
@@ -1,8 +1,6 @@
 from time import time 
-import tensorflow as tf 
 import sys 
 import json
-import torch 
 
 class TrainingFramework():
 	def __init__(self):
@@ -33,125 +31,149 @@ class TrainingFramework():
 			else:
 				logs[k] = [[s],[v]]
 
-				
+		#===============================
+		# initialization actions
+		#===============================				
+		if config["disable_tensorflow_all"]:
+			import torch 
 
+			sess = None
+		else:
+			import tensorflow as tf
 
-		gpu_options = tf.GPUOptions(allow_growth=True)
-		with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
-			model = self.createModel(sess)
-			dataloader = self.createDataloader("training")
-			# if use_validation:
-			# 	dataloader_validation = self.createDataloader("testing")
+			gpu_options = tf.GPUOptions(allow_growth=True)
+			sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+		
+		model = self.createModel(sess)
+		dataloader = self.createDataloader("training")
+		# if use_validation:
+		# 	dataloader_validation = self.createDataloader("testing")
 
-			loss = 0
-			t_load = 0
-			t_preload = 0
-			t_train = 0 
-			t_other = 0
+		loss = 0
+		t_load = 0
+		t_preload = 0
+		t_train = 0 
+		t_other = 0
+		t0 = time()
+
+		lastprogress = -1
+
+		#===============================
+		# begin: training loop
+		#===============================
+		while True:
+			t_other += time() - t0
 			t0 = time()
+			batch = self.getBatch(dataloader)
 
-			lastprogress = -1
+			# from resnet34unet import resnet34unet_v3, unet, unet_dilated
+			# with tf.compat.v1.variable_scope("test", reuse=tf.compat.v1.AUTO_REUSE):
+			# 	aaa=resnet34unet_v3(tf.convert_to_tensor(batch[0], tf.float32), True, ch_in = 3, ch_out = 4)
+			# 	pass
 
-			while True:
-				t_other += time() - t0
-				t0 = time()
-				batch = self.getBatch(dataloader)
+			t1 = time()
 
-				# from resnet34unet import resnet34unet_v3, unet, unet_dilated
-				# with tf.compat.v1.variable_scope("test", reuse=tf.compat.v1.AUTO_REUSE):
-				# 	aaa=resnet34unet_v3(tf.convert_to_tensor(batch[0], tf.float32), True, ch_in = 3, ch_out = 4)
-				# 	pass
+			if 'torch' in config["model_name"]:
+				input = torch.permute(torch.FloatTensor(batch[0]), (0,3,1,2)).to(model.device)		# need to rearrange order, N,640,640,4->N,4,640,640
+				mask = torch.permute(torch.FloatTensor(batch[1]), (0,3,1,2)).to(model.device)  # N,1,640,640
+				target = torch.permute(torch.FloatTensor(batch[2]), (0,3,1,2)).to(model.device) # N,1,640,640
+				target_normal = torch.permute(torch.FloatTensor(batch[3]), (0,3,1,2)).to(model.device) # N,2,640,640
+			
+				# Zero your gradients for every batch
+				model.optimizer_torch.zero_grad()
 
-				t1 = time()
+				# Make predictions for this batch
+				output_res = model.backboneModelTorch(input) 
+				output_seg = torch.softmax(output_res[:,0:2,:,:], dim=1)[:,0:1,:,:] # torch.softmax(output_res[:,0:2,:,:], dim=1)[0,0:1,:,:] equal to torch.softmax(output_res[0,0:2,:,:], dim=0)[0:1,:,:]
+				output_direction = output_res[:,2:4,:,:]
+				output = torch.concat([output_seg, output_direction], axis=1) # N,3,640,640
 
-				if 'torch' in config["model_name"]:
-					input = torch.permute(torch.FloatTensor(batch[0]), (0,3,1,2)) 		# need to rearrange order, N,640,640,4->N,4,640,640
-					mask = torch.permute(torch.FloatTensor(batch[1]), (0,3,1,2))  # N,1,640,640
-					target = torch.permute(torch.FloatTensor(batch[2]), (0,3,1,2)) # N,1,640,640
-					target_normal = torch.permute(torch.FloatTensor(batch[3]), (0,3,1,2)) # N,2,640,640
+				# Compute the loss and its gradients
+				lossCur = model.lossFunctionTorch(output, mask, target, target_normal)
+				lossCur.backward()
+
+				# Adjust learning weights
+				model.optimizer_torch.param_groups[0]['lr'] = lr
+				model.optimizer_torch.step()
+
+				# agument result
+				result = [lossCur.item(), torch.permute(output,(0,2,3,1)).detach().cpu().numpy(), None]
+
+			else:
+				result = self.train(batch, lr)
 				
-					# Zero your gradients for every batch
-					model.optimizer_torch.zero_grad()
+			t2 = time()
+			self.preload(dataloader, step)
+			t3 = time()
 
-					# Make predictions for this batch
-					output_res = model.resnet34_torch(input) 
-					output_seg = torch.softmax(output_res[:,0:2,:,:], dim=1)[:,0:1,:,:] # torch.softmax(output_res[:,0:2,:,:], dim=1)[0,0:1,:,:] equal to torch.softmax(output_res[0,0:2,:,:], dim=0)[0:1,:,:]
-					output_direction = output_res[:,2:4,:,:]
-					output = torch.concat([output_seg, output_direction], axis=1) # N,3,640,640
+			t_load += t1-t0 
+			t_train += t2-t1
+			t_preload += t3-t2
 
-					# Compute the loss and its gradients
-					lossCur = model.lossFunctionTorch(output, mask, target, target_normal)
-					lossCur.backward()
+			loss += self.getLoss(result)
 
-					# Adjust learning weights
-					model.optimizer_torch.param_groups[0]['lr'] = lr
-					model.optimizer_torch.step()
+			if step % 10 == 0:
+				progress = self.getProgress(step)
+				p = int((progress - int(progress)) * 50)
+				loss /= 10.0
 
-					# agument result
-					result = [lossCur.item(), torch.permute(output,(0,2,3,1)).detach().cpu().numpy(), None]
+				addlog("loss", loss, step)
 
-				else:
-					result = self.train(batch, lr)
+				s = ""
+				ks = sorted(self.kv.keys())
+				for k in ks:
+					v = self.kv[k]
+					if v[1] > 0:
+						s += " %s:%E " % (k, v[0] / float(v[1]))
+						addlog(k, v[0] / float(v[1]), step)
+						self.kv[k] = [0,0]
+
+				sys.stdout.write("\rstep %d epoch:%.2f "% (step, progress) + ">" * p + "-" * (51-p) + " loss %f time %f %f %f %f " % (loss, t_preload, t_load, t_train, t_other - t_preload - t_load - t_train) + s )
+				sys.stdout.flush()	
+
+				p = int((progress - int(progress)) * 100000)
+				if int(progress) != int(lastprogress):
+					print("time per epoch", t_other)
+					print("eta", t_other * (maxstep - step) / max(1, (step - last_step)) / 3600.0 )
+					last_step = step
+					t_load = 0
+					t_preload = 0
+					t_train = 0 
+					t_other = 0
+
 					
-				t2 = time()
-				self.preload(dataloader, step)
-				t3 = time()
 
-				t_load += t1-t0 
-				t_train += t2-t1
-				t_preload += t3-t2
+				loss = 0
+				lastprogress = progress 
 
-				loss += self.getLoss(result)
+			self.saveModel(step)
+			self.visualization(step, result, batch)
+			
+			for i in range(len(lr_decay_step)):
+				if step == lr_decay_step[i]:
+					lr = lr * lr_decay[i]
 
-				if step % 10 == 0:
-					progress = self.getProgress(step)
-					p = int((progress - int(progress)) * 50)
-					loss /= 10.0
+			if step == maxstep + 1:
+				break
+			
+			if step % 1000 == 0 and step > 0:
+				json.dump(logs, open(logfile, "w"), indent=2)
 
-					addlog("loss", loss, step)
-
-					s = ""
-					ks = sorted(self.kv.keys())
-					for k in ks:
-						v = self.kv[k]
-						if v[1] > 0:
-							s += " %s:%E " % (k, v[0] / float(v[1]))
-							addlog(k, v[0] / float(v[1]), step)
-							self.kv[k] = [0,0]
-
-					sys.stdout.write("\rstep %d epoch:%.2f "% (step, progress) + ">" * p + "-" * (51-p) + " loss %f time %f %f %f %f " % (loss, t_preload, t_load, t_train, t_other - t_preload - t_load - t_train) + s )
-					sys.stdout.flush()	
-
-					p = int((progress - int(progress)) * 100000)
-					if int(progress) != int(lastprogress):
-						print("time per epoch", t_other)
-						print("eta", t_other * (maxstep - step) / max(1, (step - last_step)) / 3600.0 )
-						last_step = step
-						t_load = 0
-						t_preload = 0
-						t_train = 0 
-						t_other = 0
-
-						
-
-					loss = 0
-					lastprogress = progress 
-
-				self.saveModel(step)
-				self.visualization(step, result, batch)
-				
-				for i in range(len(lr_decay_step)):
-					if step == lr_decay_step[i]:
-						lr = lr * lr_decay[i]
-
-				if step == maxstep + 1:
-					break
-				
-				if step % 1000 == 0 and step > 0:
-					json.dump(logs, open(logfile, "w"), indent=2)
-
-				step += 1
-
+			step += 1
+		#===============================
+		# end: training loop
+		#===============================
+			
+		#===============================
+		# shutdown actions
+		#===============================
+		if config["disable_tensorflow_all"]:
+			pass
+		else:
+			sess.close()
+		#===============================
+		#===============================
+			
 	# features
 	def logvalue(self, k, v):
 		if k in self.kv:
--- code_torch/laneAndDirectionExtraction/model.py
+++ code_torch/laneAndDirectionExtraction/model.py
@@ -2,28 +2,33 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.getcwd()))
 
-import tensorflow as tf 
 import sys 
-from resnet34unet import resnet34unet_v3, unet, unet_dilated
-
-from resnet34unet_torch import UnetResnet34
-import torch
 
 class LaneModel():
 	def __init__(self, sess, size = 640, batchsize = 4, sdmap = False, backbone = "resnet34v3"):
-		self.sess = sess 
-		self.batchsize = batchsize
-		self.input = tf.placeholder(dtype = tf.float32, shape = [None, size, size, 3])
-		self.target = tf.placeholder(dtype = tf.float32, shape = [None, size, size, 1])
-		self.target_normal = tf.placeholder(dtype = tf.float32, shape = [None, size, size, 2])
-		self.sdmap = tf.placeholder(dtype = tf.float32, shape = [None, size, size, 1])
-		self.backbone = backbone
-		self.mask = tf.placeholder(dtype = tf.float32, shape = [None, size, size, 1])
-		
-		
-		self.lr = tf.placeholder(dtype=tf.float32)
-		self.is_training = tf.placeholder(tf.bool, name="istraining")
+		if sess is not None:
+			DISABLE_TENSORFLOW_ALL = False
+		else:
+			DISABLE_TENSORFLOW_ALL = True
 
+		if not DISABLE_TENSORFLOW_ALL:
+			import tensorflow as tf 
+			from resnet34unet import resnet34unet_v3, unet, unet_dilated
+
+			self.sess = sess 
+			self.input = tf.placeholder(dtype = tf.float32, shape = [None, size, size, 3])
+			self.target = tf.placeholder(dtype = tf.float32, shape = [None, size, size, 1])
+			self.target_normal = tf.placeholder(dtype = tf.float32, shape = [None, size, size, 2])
+			self.sdmap = tf.placeholder(dtype = tf.float32, shape = [None, size, size, 1])
+			self.mask = tf.placeholder(dtype = tf.float32, shape = [None, size, size, 1])
+			self.lr = tf.placeholder(dtype=tf.float32)
+			self.is_training = tf.placeholder(tf.bool, name="istraining")
+		else:
+			import torch
+			from resnet34unet_torch import UnetResnet34
+
+		self.backbone = backbone
+		self.batchsize = batchsize
 		self.hassdmap = sdmap
 
 		# if sdmap:
@@ -35,13 +40,15 @@ class LaneModel():
 			# pytorch model
 			#==========================
 			device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-			self.resnet34_torch = UnetResnet34().to(device)
+			self.backboneModelTorch = UnetResnet34().to(device)
 
-			self.optimizer_torch = torch.optim.Adam(self.resnet34_torch.parameters(), lr=0.001)
+			self.optimizer_torch = torch.optim.Adam(self.backboneModelTorch.parameters(), lr=0.001)
+			self.device = device
 			#==========================
 			#==========================
 
-			output = resnet34unet_v3(self.input, self.is_training, ch_in = 3, ch_out = 4)
+			if not DISABLE_TENSORFLOW_ALL:
+				output = resnet34unet_v3(self.input, self.is_training, ch_in = 3, ch_out = 4)
 
 		elif self.backbone == "resnet34v3":
 			output = resnet34unet_v3(self.input, self.is_training, ch_in = 3, ch_out = 4)
@@ -52,26 +59,30 @@ class LaneModel():
 		elif self.backbone == "unet_dilated":
 			output = unet_dilated(self.input, self.is_training, ch_in = 3, ch_out = 4)
 
-		self.loss = self.singlescaleloss(output[:,:,:,0:2], self.target, self.mask)
-		self.loss += tf.reduce_mean(self.mask * tf.square(self.target_normal - output[:,:,:,2:4]))
+		if not DISABLE_TENSORFLOW_ALL:
+			self.loss = self.singlescaleloss(output[:,:,:,0:2], self.target, self.mask)
+			self.loss += tf.reduce_mean(self.mask * tf.square(self.target_normal - output[:,:,:,2:4]))
 
-		#self.loss += self.celoss(output[:,:,:,2:4], self.target_t, self.mask)
-		
-		output_seg = tf.nn.softmax(output[:,:,:,0:2])[:,:,:,0:1]
-		output_direction = output[:,:,:,2:4]
+			#self.loss += self.celoss(output[:,:,:,2:4], self.target_t, self.mask)
+			
+			output_seg = tf.nn.softmax(output[:,:,:,0:2])[:,:,:,0:1]
+			output_direction = output[:,:,:,2:4]
 
-		self.output = tf.concat([output_seg, output_direction], axis=3)
+			self.output = tf.concat([output_seg, output_direction], axis=3)
 
-		self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss)
-		
-		
-		self.sess.run(tf.global_variables_initializer())
-		self.saver = tf.train.Saver(max_to_keep=21)
+			self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss)
+			
+			
+			self.sess.run(tf.global_variables_initializer())
+			self.saver = tf.train.Saver(max_to_keep=21)
 	
+		self.DISABLE_TENSORFLOW_ALL = DISABLE_TENSORFLOW_ALL
+
 	#==========================
 	# pytorch loss
 	#==========================
 	def lossFunctionTorch(self, output, mask, target, target_normal):
+		import torch
 
 		t1 = target # N,1,640,640
 		p = output # N,3,640,640, 1st is seg, 2nd 3rd direction
@@ -101,6 +112,8 @@ class LaneModel():
 	#==========================
 
 	def singlescaleloss(self, p, target, mask):
+		import tensorflow as tf
+
 		t1 = target
 		
 		def ce_loss(p, t):
@@ -125,6 +138,8 @@ class LaneModel():
 		return loss 
 
 	def celoss(self, p, target, mask):
+		import tensorflow as tf
+
 		t1 = target
 		def ce_loss(p, t):
 			#t = tf.concat([t,1-t], axis=3)
@@ -176,8 +191,16 @@ class LaneModel():
 		return self.sess.run(ops, feed_dict=feed_dict)
 
 	def saveModel(self, path):
-		self.saver.save(self.sess, path)
+		if not self.DISABLE_TENSORFLOW_ALL:
+			self.saver.save(self.sess, path)
+		else:
+			import torch
+			torch.save(self.backboneModelTorch.state_dict(), path+'.pth')
 
 	def restoreModel(self, path):
-		self.saver.restore(self.sess, path)
+		if not self.DISABLE_TENSORFLOW_ALL:
+			self.saver.restore(self.sess, path)
+		else:
+			import torch
+			self.backboneModelTorch.load_state_dict(torch.load(path+'.pth'))
 
