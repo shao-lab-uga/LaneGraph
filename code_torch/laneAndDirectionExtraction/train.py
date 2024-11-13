936a1eeb643f58ee36110633c2bfc196144754f0
Yunli Shao <yunli.shao@uga.edu>
2024-11-13 01:41:13 -0500

    change saving figure to skip sdmap if it is disabled every time create a tmp folder based on current time put model, figures, log under same folder put a variable to determine how often to save images

--- code_torch/laneAndDirectionExtraction/train.py
+++ code_torch/laneAndDirectionExtraction/train.py
@@ -21,6 +21,11 @@ import math
 import json 
 import shutil
 
+from datetime import datetime
+
+DISABLE_TENSORFLOW_ALL= True # global flag to disable tensorflow all
+SAVE_FIGURE_INTERVAL = 50 # save every XX steps
+
 class Train(TrainingFramework):
 	def __init__(self):
 		self.image_size = 640
@@ -40,15 +45,18 @@ class Train(TrainingFramework):
 		if self.use_sdmap:
 			self.instance += "_withsdmap"
 
-		self.modelfolder = "model" + self.instance
-		self.validationfolder = "validation" + self.instance
-			
-		# remove all previous results
-		if os.path.exists(self.validationfolder):
-			shutil.rmtree(self.validationfolder)
+		savePath = os.path.join(os.getcwd(), 'LogTmp', '{}'.format(datetime.now().strftime("%Y_%m_%d_%H_%M_%S")))
+		os.makedirs(savePath, exist_ok=True)
 
-		Popen("mkdir " + self.modelfolder, shell=True).wait()
-		Popen("mkdir " + self.validationfolder, shell=True).wait()
+		self.modelfolder = savePath
+		self.validationfolder = savePath
+			
+		# # remove all previous results
+		# if os.path.exists(self.validationfolder):
+		# 	shutil.rmtree(self.validationfolder)
+
+		# Popen("mkdir " + self.modelfolder, shell=True).wait()
+		# Popen("mkdir " + self.validationfolder, shell=True).wait()
 
 
 		self.counter = 0
@@ -90,47 +98,48 @@ class Train(TrainingFramework):
 			exit()
 
 		return result[0]
-
+	
 	def getProgress(self, step):
 		return step / float(self.epochsize)
 
 	def saveModel(self, step):
 		if step % (self.epochsize * 10) == 0:
-			self.model.saveModel(self.modelfolder + "/model%d" % (step // (self.epochsize)))
+			self.model.saveModel(os.path.join(self.modelfolder, "model_epoch_%d" % (step // (self.epochsize))))
 		return False
 
 	def visualization(self, step, result = None, batch = None):
 		direction_img = np.zeros((self.image_size, self.image_size, 3))
 
-		saveFigIntvl = 10
-		if step % saveFigIntvl == 0:
+		if step % SAVE_FIGURE_INTERVAL == 0:
 			# ind = ((step // 100) * self.batch_size) % 128
-			ind = ((step//saveFigIntvl) * self.batch_size)
+			ind = ((step//SAVE_FIGURE_INTERVAL) * self.batch_size)
 
 			#batch[3] = np.clip(batch[3], -1, 1)
 			#result[1] = np.clip(result[1], -1, 1)
 
 			for i in range(self.batch_size):
-				Image.fromarray(((batch[0][i,:,:,:] + 0.5) * 255).astype(np.uint8) ).save(self.validationfolder + "/input%d.jpg" % (ind+i))
-				Image.fromarray(((batch[1][i,:,:,0]) * 255).astype(np.uint8) ).save(self.validationfolder + "/mask%d.jpg" % (ind+i))
-				Image.fromarray(((batch[2][i,:,:,0]) * 255).astype(np.uint8) ).save(self.validationfolder + "/target%d.jpg" % (ind+i))
-				
-				Image.fromarray(((batch[4][i,:,:,0]) * 255).astype(np.uint8) ).save(self.validationfolder + "/sdmap%d.jpg" % (ind+i))
+				idStr = '_{}_{}_{}'.format(int(step // (self.epochsize)), step, i) # convention: epoch, step, index of figure
+
+				Image.fromarray(((batch[0][i,:,:,:] + 0.5) * 255).astype(np.uint8) ).save(os.path.join(self.validationfolder, "input{}.jpg".format(idStr)))
+				Image.fromarray(((batch[1][i,:,:,0]) * 255).astype(np.uint8) ).save(os.path.join(self.validationfolder, "mask{}.jpg".format(idStr)))
+				Image.fromarray(((batch[2][i,:,:,0]) * 255).astype(np.uint8) ).save(os.path.join(self.validationfolder, "target{}.jpg".format(idStr)))
+				if self.use_sdmap:
+					Image.fromarray(((batch[4][i,:,:,0]) * 255).astype(np.uint8) ).save(os.path.join(self.validationfolder, "sdmap{}.jpg".format(idStr)))
 				
 
 				direction_img[:,:,2] = batch[3][i,:,:,0] * 127 + 127
 				direction_img[:,:,1] = batch[3][i,:,:,1] * 127 + 127
 				direction_img[:,:,0] = 127
 
-				Image.fromarray(direction_img.astype(np.uint8)).save(self.validationfolder + "/targe_direction%d.jpg" % (ind+i))
+				Image.fromarray(direction_img.astype(np.uint8)).save(os.path.join(self.validationfolder, "targe_direction{}.jpg".format(idStr)))
 				
-				Image.fromarray(((result[1][i,:,:,0]) * 255).astype(np.uint8) ).save(self.validationfolder + "/output%d.jpg" % (ind+i))
+				Image.fromarray(((result[1][i,:,:,0]) * 255).astype(np.uint8) ).save(os.path.join(self.validationfolder, "output{}.jpg".format(idStr)))
 				
 				direction_img[:,:,2] = np.clip(result[1][i,:,:,1],-1,1) * 127 + 127
 				direction_img[:,:,1] = np.clip(result[1][i,:,:,2],-1,1) * 127 + 127
 				direction_img[:,:,0] = 127
 				
-				Image.fromarray(direction_img.astype(np.uint8) ).save(self.validationfolder + "/output_direction%d.jpg" % (ind+i))
+				Image.fromarray(direction_img.astype(np.uint8) ).save(os.path.join(self.validationfolder, "output_direction{}.jpg".format(idStr)))
 				
 			
 		return False
@@ -147,8 +156,10 @@ if __name__ == "__main__":
 	config["step_init"] = 0
 	config["step_max"] = epochsisze * 500 + 1
 	config["use_validation"] = False
-	config["logfile"] = "log_lane_%s.json" % trainer.instance
+	config["logfile"] = os.path.join(trainer.validationfolder,"log_lane_%s.json" % trainer.instance)
 	config["model_name"] = trainer.backbone
+
+	config["disable_tensorflow_all"] = DISABLE_TENSORFLOW_ALL
 
 	trainer.run(config)
 
